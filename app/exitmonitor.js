9eaefc0fa6af3c0f33706e8beca8e473b1c5515b
rsmith-finix <rsmith@finixcreative.com>
2016-05-25 16:15:15 -0700

    App - bootstrap
    Initialize site with bootstrap components and files

---
+++ app/exitmonitor.js
@@ -0,0 +1,36 @@
+window.onload = function(){
+	var exitCount,
+		template,
+		exitMonitor;
+	exitCount = 0;
+	template = "<div id='exitmonitor' class='configpanel white-txt pad-150-20 center full'><h3>Wait up!</h3><button id='dismiss' class='pad-20'>Nah...</button></div>";
+	exitMonitor = function(){
+		if(exitCount === 0){
+			$('#content').prepend(template);
+			exitMonitorDismiss();
+			exitCount++;
+			return;
+		} else if(exitCount === 1){
+			exitCount++;
+			console.log("Exitmonitor disabled\nExits: " + exitCount)
+			return;
+		} else if(exitCount > 1){
+			exitCount++;
+			console.log("Exits: " + exitCount)
+			return;
+		} else {
+			return;
+		}
+	};
+	$('html').mouseleave(exitMonitor);
+};
+function exitMonitorDismiss(){
+	var dismiss;
+	dismiss = function(){
+		$(this).closest('#exitmonitor').hide();
+		console.log("Exitmonitor dismissed");
+	};
+	$(document).ready(function(){
+		$('#dismiss').click(dismiss);		
+	});
+};
---
+++ app/modules/contact/contact-drv.js
@@ -0,0 +1,10 @@
+angular.module('coreMod')
+	.directive('contact', function(){
+		return {
+			restrict: 'E',
+			templateUrl: '/web/app/modules/contact/contact.html',
+			controller: 'ClientController',
+			controllerAs: 'client',
+		};
+	})
+;
