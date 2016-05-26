80727f66006acaf2b385c44d9ac61befcdd20654
rsmith-finix <rsmith@finixcreative.com>
2016-05-26 09:52:05 -0700

    Header - directive scroll
    Add scroll change to header directive

--- app/modules/header/header-drv.js
+++ app/modules/header/header-drv.js
@@ -1,8 +1,19 @@
 angular.module("coreMod")
 	.directive("fxheader", function(){
+		function headerScroll(){
+			$(window).on('scroll',function(){
+				distance = Math.round($(window).scrollTop());
+				if (distance > 120) {
+					$('header').addClass('white-bg');
+				} else {
+					$('header').removeClass('white-bg');
+				}
+			});
+		};
 		return {
 			restrict: "E",
-			templateUrl: "app/modules/header/header.html"
+			templateUrl: "app/modules/header/header.html",
+			link: headerScroll
 		};
 	})
 ;
